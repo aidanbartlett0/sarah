662b2b613a456bf17b1a8f0f05e09d333af8622f
Aidan <aidanb04@uw.edu>
2025-04-03 18:33:14 -0700

    update

--- slideshow.js
+++ slideshow.js
@@ -1,19 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const images = [
-        "images/IMG_2544.jpeg",
-        "images/IMG_2262.jpg",
-        "images/IMG_2544.jpeg",
-        "images/IMG_2262.jpg"
-    ];
+const meow = document.getElementById('papayameow')
 
-    let index = 0;
-    const slide = document.getElementById("slide");
-
-    function nextImage() {
-        index = (index + 1) % images.length;
-        slide.src = images[index];
-    }
-
-    // Change image every 3 seconds
-    setInterval(nextImage, 3000);
+meow.addEventListener("click", function () {
+    const audio = new Audio("freejazz.wav");
+    audio.play();
+// lets get different papaya tones and pictures and on click it gives a new pic and soundbyte. 
+// ranking based on enjoyment
 });
