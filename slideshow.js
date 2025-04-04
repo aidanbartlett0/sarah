464570553b10ca82c9eee097b452af1ac7e1ffc4
Aidan <aidanb04@uw.edu>
2025-04-03 19:00:31 -0700

    d

--- slideshow.js
+++ slideshow.js
@@ -1,7 +1,7 @@
 const meow = document.getElementById('papayameow')
 
 meow.addEventListener("click", function () {
-    const audio = new Audio("freejazz.wav");
+    const audio = new Audio("sounds/freejazz.wav");
     audio.play();
 // lets get different papaya tones and pictures and on click it gives a new pic and soundbyte. 
 // ranking based on enjoyment
